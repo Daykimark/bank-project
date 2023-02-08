925ce1ce948b97cb8cf3012af2a5a1505221a02b
AlexeyOkolelov <okolelov9805@bk.ru>
2023-02-08 15:23:37 +0000

    Подключил к эврике сервер antifraud[bk-12]

--- antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
@@ -2,9 +2,10 @@ package com.bank.antifraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-// TODO AntifraudApplication переименуй в AntiFraudApplication
 public class AntifraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntifraudApplication.class, args);
